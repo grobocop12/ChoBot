f8f528d6f501c850df30b9b548ec2321e3d9cd9e
nynon <nynonmaslo@gmail.com>
2021-11-25 17:39:17 +0100

    changed botroomid

--- src/discordBot/events/utils/stream.js
+++ src/discordBot/events/utils/stream.js
@@ -1,5 +1,5 @@
 async function fetchStreamChannel(client) {
-  const botRoomId = `887380369282830396`;
+  const botRoomId = `913468442118606908`;
   const guildId = "87143400691728384";
   const guild = await client.guilds.fetch(guildId);
   return guild.channels.fetch(botRoomId);
